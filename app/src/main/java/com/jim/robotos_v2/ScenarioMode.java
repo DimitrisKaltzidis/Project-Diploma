6dc2960b8dc88e9a47611e384a37fc88a45d4a00
Jim <icsd11053@icsd.aegean.gr>
2016-03-18 01:21:28 +0200

    Add UI components on ScenarioMode layout and onClick handler methods

--- app/src/main/java/com/jim/robotos_v2/ScenarioMode.java
+++ app/src/main/java/com/jim/robotos_v2/ScenarioMode.java
@@ -2,6 +2,7 @@ package com.jim.robotos_v2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
 
 public class ScenarioMode extends AppCompatActivity {
@@ -11,5 +12,12 @@ public class ScenarioMode extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenario_mode);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+
+    public void previewColorClicked(View view) {
+    }
+
+    public void nextActivityClicked(View view) {
     }
 }
